74db0bedfcfc297e8f55a7544875c7d4a96edd51
trist <tvanCaspel@roc-dev.com>
2020-10-19 15:15:06 +0200

    H13.1

---
+++ src/H13.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.applet.*;
+
+public class H13 extends Applet{
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.cyan);
+        tekenDriehoek(g,90,50,50,10,25,50);
+
+
+    }
+    void tekenDriehoek( Graphics g, int x1, int y1, int x2, int y2, int x3, int y3) {
+        g.drawLine(x1,y1,x2,y2);
+        g.drawLine(x2,y2,x3,y3);
+        g.drawLine(x3,y3,x1,y1);
+
+    }
+}
