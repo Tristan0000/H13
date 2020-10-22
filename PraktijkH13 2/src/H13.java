2fbd45be55e320606a01c21eba2554b5ebc92fb8
Tristan0000 <70886734+Tristan0000@users.noreply.github.com>
2020-10-22 09:09:15 +0200

    Add files via upload

---
+++ PraktijkH13 2/src/H13.java
@@ -0,0 +1,26 @@
+import java.applet.*;
+import java.awt.*;
+
+public class H13 extends Applet {
+
+    public void paint(Graphics g) {
+        setBackground(Color.cyan);
+        boom(g,50,90);
+    }
+
+    void boom(Graphics g, int x, int y) {
+        int teller;
+        int xcopy = x;
+        for(teller = 0; teller < 10; teller++) {
+            g.setColor(Color.black);
+            g.fillRect(x,y,20,100);
+            g.setColor(Color.green);
+            g.fillArc(x-35,y-70,90,90,0,360);
+            x += 100;
+            if (teller == 4) {
+                y += 100;
+                x = xcopy + 50;
+            }
+        }
+    }
+}
